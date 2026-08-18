af32057a4bca91740cf9ce489cee1a8ecdd403e7
MangoTarso <nachox97@hotmail.com>
2026-08-17 22:04:49 -0400

    Changed module to allow amount of clicks

--- standalone.py
+++ standalone.py
@@ -5,15 +5,24 @@ class dogClicker:
 
     def __init__(self, path):
         self.sound = path
+        self._times = 1
+
+    def setTimes(self, number):
+        self._times = number
     
     def play(self):
-        playsound(self.sound)
+        if self.sound != "":
+            for i in range(self._times):
+                playsound(self.sound, False)
+                tm.sleep(0.175)
 
     def change(self, new_sound):
         self.sound = new_sound
     
 if __name__ == "__main__":
-    click = dogClicker("dog-clicker.mp3")
+    click = dogClicker("")
+    times = input("Set times: ")
+    click.setTimes(int(times))
 
     while True:
         click.play()
